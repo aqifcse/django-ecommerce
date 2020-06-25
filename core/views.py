105ce025467e14df921e57620259c5ad72d8bb75
Syed Towfiqur Rahim <5446400-aqifcse@users.noreply.gitlab.com>
2020-06-25 21:19:14 +0600

    25/06/2020 commit

--- core/views.py
+++ core/views.py
@@ -1,8 +1,6 @@
 from django.shortcuts import render
-from .models import Item
 
-def item_list(request):
-    context = {
-        'items': Item.objects.all()
-    }
-    return render(request, "item_list.html", context)
+def index (request): 
+    return render(request, 'index.html')
+
+

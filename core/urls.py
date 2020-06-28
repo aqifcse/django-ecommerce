6a054e6196b77f0c3cb250558ce2ed759706b8e0
Syed Towfiqur Rahim <5446400-aqifcse@users.noreply.gitlab.com>
2020-06-28 18:43:31 +0600

    28/06/2020 commit

--- core/urls.py
+++ core/urls.py
@@ -1,8 +1,28 @@
 from django.urls import path
-from .views import index
+from .views import (
+    index,
+    blog,
+    cart,
+    category,
+    checkout,
+    contact,
+    elements,
+    singleblog,
+    singleproduct,
+    tracking
+)
 
 app_name= 'core'
 
 urlpatterns = [
-    path('', index, name='index')  
+    path('', index, name='index'),
+    path('blog/', blog, name='blog'),
+    path('cart/', cart, name='cart'),
+    path('category/', category, name='category'),
+    path('checkout/', checkout, name='checkout'),
+    path('contact/', contact, name='contact'),
+    path('elements/', elements, name='elements'),
+    path('single-blog/', singleblog, name='singleblog'),
+    path('single-product/', singleproduct, name='singleproduct'),
+    path('tracking/', tracking, name='tracking')     
 ]
--- core/views.py
+++ core/views.py
@@ -1,6 +1,31 @@
 from django.shortcuts import render
 
-def index (request): 
+def index (request):
     return render(request, 'index.html')
 
+def blog (request): 
+    return render(request, 'blog.html')
 
+def cart (request): 
+    return render(request, 'cart.html')
+
+def category (request): 
+    return render(request, 'category.html')
+
+def checkout (request): 
+    return render(request, 'checkout.html')
+
+def contact (request): 
+    return render(request, 'contact.html')
+
+def elements (request): 
+    return render(request, 'elements.html')
+
+def singleblog (request): 
+    return render(request, 'single-blog.html')
+
+def singleproduct (request): 
+    return render(request, 'single-product.html')
+
+def tracking (request): 
+    return render(request, 'tracking.html')
